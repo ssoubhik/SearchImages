622a8d1e752c2cddf01cd3eb1f9c5e165ce8fd49
Soubhik Sarkhel <soubhik@rhymetech.io>
2022-09-23 15:04:21 +0530

    Initial Commit

---
+++ SearchImage/SearchImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchImageApp.swift
+//  SearchImage
+//
+//  Created by Rhymetech on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
